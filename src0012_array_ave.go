0516f4c963c86cd0667de23b29e759e74198fba8
icasimpan <ismael.angelo@casimpan.com>
2014-08-26 17:10:42 +0800

    added array average

---
+++ src0012_array_ave.go
@@ -0,0 +1,22 @@
+/* Print average of above scores (use arrays): 98, 93, 77, 82, 83*/
+
+package main
+
+import "fmt"
+
+func main() {
+  var x [5]float64
+  x[0] = 98
+  x[1] = 93
+  x[2] = 77
+  x[3] = 82
+  x[4] = 83
+
+  var total float64;
+
+  for i:=0; i<=4; i++ {
+    total += x[i]    
+  }
+
+  fmt.Println("Average is: ", total/5)
+}//END: main()
