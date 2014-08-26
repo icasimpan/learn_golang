68aef652d6ae858e8daa0e3c7ef7cf8f8931e3ba
icasimpan <ismael.angelo@casimpan.com>
2014-08-26 16:40:29 +0800

    added switch example

---
+++ src0010_switch.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("***Number in words***") 
+  for i:=0; i<=9; i++ {
+    switch i {
+      case 0: fmt.Println("Zero")
+      case 1: fmt.Println("One")
+      case 2: fmt.Println("Two")
+      case 3: fmt.Println("Three")
+      case 4: fmt.Println("Four")
+      case 5: fmt.Println("Five")
+      case 6: fmt.Println("Six")
+      case 7: fmt.Println("Seven")
+      case 8: fmt.Println("Eight")
+      case 9: fmt.Println("Nine")
+      default: fmt.Println("__Unrecognized__")
+    }// END: switch
+  }// END: for
+}// END main()
